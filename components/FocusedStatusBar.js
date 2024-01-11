f75da1d92a57d13087d8d64c2a91bdcaf53d1087
igudy <goodnessigunma1@gmail.com>
2024-01-11 23:50:02 +0100

    NFTCard started and other components

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -1,10 +1,8 @@
-import { View, Text } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 import React from "react";
+import { useIsFocused } from "@react-navigation/native";
 
-export default function FocusedStatusBar() {
-  return (
-    <View>
-      <Text>FocusedStatusBar</Text>
-    </View>
-  );
+export default function FocusedStatusBar(props) {
+  const isFocused = useIsFocused();
+  return isFocused ? <StatusBar animated={true} {...props} /> : null;
 }
---
+++ components/SubInfo.js
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export const NFTTitle = () => {};
+
+export const EthPrice = () => {};
+
+export const ImageCmp = () => {};
+
+export const People = () => {};
+
+export const EndDate = () => {};
+
+export const SubInfo = () => {
+  return (
+    <View>
+      <Text>SubInfo</Text>
+    </View>
+  );
+};
