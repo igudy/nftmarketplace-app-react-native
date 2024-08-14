fbf110eddb9399fe13740a14c06d221390bc03c8
igudy <goodnessigunma1@gmail.com>
2024-08-14 15:57:42 +0100

    feat: status bar

---
+++ components/DetailsBid.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const DetailsBid = () => {
+  return (
+    <View>
+      <Text>Details Bid</Text>
+    </View>
+  );
+};
+
+export default DetailsBid;
--- components/index.js
+++ components/index.js
@@ -2,5 +2,17 @@ import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
 import { RectButton, CircleButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
 
-export { HomeHeader, NFTCard, FocusedStatusBar, RectButton, CircleButton };
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  RectButton,
+  CircleButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
